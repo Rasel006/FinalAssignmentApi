3ebb2952db4405133f55da3327d279d2eb3216b7
Rasel006 <rizurasel.cse@gmail.com>
2024-05-01 00:17:41 +0600

    error update

--- MdRasel/settings.py
+++ MdRasel/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-49t00*8*6+fd&(b2i=(kn3g80o((-=m_r0)*6==jykg)5dpc6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
